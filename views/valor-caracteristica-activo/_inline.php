6d34373885645128aac6f5420383358bb51e9078
gdiazderadaa <g.diazderada@outlook.com>
2016-02-01 11:37:00 +0000

    Fixed issue with related models

--- views/valor-caracteristica-activo/_inline.php
+++ views/valor-caracteristica-activo/_inline.php
@@ -17,7 +17,7 @@ if( $model->primaryKey )
 {
     // you must define an attribute called "data-dynamic-relation-remove-route" if you plan to allow inline deletion of models from the form.
     $removeAttr = 'data-dynamic-relation-remove-route="' . 
-        Url::toRoute(['valor-caracteristica-activo/delete', 'ValorCaracteristicaActivoID' => $model->ValorCaracteristicaActivoID]) . '"';
+        Url::toRoute(['valor-caracteristica-activo/delete', 'id' => $model->ValorCaracteristicaActivoID]) . '"';
     $frag = "ValorCaracteristicaActivo[{$model->primaryKey}]";
 }
 else
@@ -30,27 +30,24 @@ else
 ?>
 
 <div class="ValorCaracteristicaActivo-form form" <?= $removeAttr; ?>>
-    <div class="basura">
-        <?php print_r( $model->primaryKey ); ?>
-    </div>
     <?php
         if ($model->isNewRecord){
     ?>
-            <select id="<?php $frag.'[Nombre]' ?>" class="form-control">
+            <select name="<?php echo $frag.'[CaracteristicaID]' ?>" class="form-control">
                 <?php
                     foreach ($model->getCaracteristicas() as $id => $nombre) {
-                        echo '<option>'.$nombre.'</option>';
+                        echo '<option value="'.$id.'">'.$nombre.'</option>';
                     }
                 ?>
             </select>  
-            <input type="text" class="form-control" placeholder="Valor" name="<?php $frag.'[Valor]' ?>" value="" />
+            <input type="text" class="form-control" placeholder="Valor" name="<?php echo $frag.'[Valor]' ?>" value="" />
     <?php        
         }
         else{
                 ?>
                 <div class="input-group">
                     <span class="input-group-addon"><?php  echo $model->caracteristica->Nombre ?></span>
-                    <input class="form-control" type="text" name="<?php $frag.'[Valor]' ?>" value="<?php echo $model->Valor ?>"/>
+                    <input class="form-control" type="text" name="<?php echo $frag.'[Valor]' ?>" value="<?php echo $model->Valor ?>"/>
                 </div>  
             <?php  
         }
